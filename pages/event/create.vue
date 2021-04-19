8202df378274a9541b79e8c6c59e7f288bb43f9d
alvin159951 <alvin159951@users.noreply.github.com>
2021-04-19 16:33:35 -0700

    Finished Lesson

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Event Listing"
+    };
+  },
+  asyncData(context) {
+    return context.$axios.get("http://localhost:3001/events").then(response => {
+      return {
+        events: response.data
+      };
+    });
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        real-world-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
-
 <script>
-export default {}
+import EventCard from "@/components/EventCard.vue";
+import { mapState } from "vuex";
+export default {
+  head() {
+    return {
+      title: "Event Listing"
+    };
+  },
+  async fetch(context) {
+    try {
+      await context.store.dispatch("events/fetchEvents");
+    } catch (e) {
+      context.error({ statusCode: 503, message: "Unable to fetch events" });
+    }
+  },
+  components: {
+    EventCard
+  },
+  computed: mapState({
+    events: state => state.events.events
+  })
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
